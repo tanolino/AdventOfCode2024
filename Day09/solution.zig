be8e86924c0ccfa3f7356a3422495c2e64f1f143
Nathanael Krauße <taniga_danae@gmx.de>
2024-12-09 22:17:11 +0100

    [ADD] Day 9 Part 2

--- Day09/solution.zig
+++ Day09/solution.zig
@@ -62,33 +62,40 @@ fn solve(filename: []const u8) !void {
     const content = try read_all_file(filename, &alloc);
     defer alloc.free(content);
 
-    const result = resolve(content);
-
-    // Some debugging
-    if (false) {
-        const cout = std.io.getStdOut().writer();
-        try cout.print("\t", .{});
-        for (content) |c| {
-            if (c >= 0) {
-                try cout.print("{d}", .{c});
-            } else {
-                try cout.print("-", .{});
-            }
-        }
+    const copy = try alloc.alloc(i32, content.len);
+    defer alloc.free(copy);
+    for (0..content.len) |i| {
+        copy[i] = content[i];
     }
 
+    const result = resolve(content);
+    const result2 = try resolve2(copy);
+
     const cout = std.io.getStdOut().writer();
-    try cout.print("File: \"{s}\" Result: {d}\n", .{ filename, result });
+    try cout.print("File: \"{s}\" Result: {d} | {d}\n", .{ filename, result, result2 });
 }
 
 pub fn main() anyerror!void {
     // p1: 1928
-    // p2:
+    // p2: 2858
     try solve("part1.example.txt");
 
     // p1: 6200294120911
-    // p2:
+    // p2: 6227018762750
     try solve("part1.test.txt");
+}
+
+fn print_content(content: []const i32) !void {
+    const cout = std.io.getStdOut().writer();
+    try cout.print("\t", .{});
+    for (content) |c| {
+        if (c >= 0) {
+            try cout.print("{d}", .{c});
+        } else {
+            try cout.print("-", .{});
+        }
+    }
+    try cout.print("\n", .{});
 }
 
 fn resolve(data: []i32) usize {
@@ -123,4 +130,71 @@ fn resolve(data: []i32) usize {
         checksum += num * i;
     }
     return checksum;
+}
+
+fn resolve2(data: []i32) !usize {
+    {
+        // try print_content(data);
+        var i: i32 = std.mem.max(i32, data);
+        while (i >= 0) {
+            one_move(data, i);
+            // try print_content(data);
+            i -= 1;
+        }
+    }
+
+    // Checksum
+    var checksum: usize = 0;
+    for (0..data.len) |i| {
+        const val = data[i];
+        if (val > 0) {
+            const num: usize = @intCast(val);
+            checksum += num * i;
+        }
+    }
+    return checksum;
+}
+
+fn one_move(data: []i32, value: i32) void {
+    var beg: usize = data.len;
+    var end: usize = 0;
+
+    // Maybe optimize
+    for (0..data.len) |i| {
+        if (data[i] == value) {
+            beg = @min(beg, i);
+            end = @max(end, i);
+        }
+    }
+
+    if (beg > end) {
+        return;
+    }
+
+    const data_size = 1 + end - beg;
+
+    var free_beg: usize = 0;
+    while (free_beg < beg) {
+        if (data[free_beg] >= 0) {
+            free_beg += 1;
+            continue;
+        }
+
+        var free_end = free_beg + 1;
+        while (data[free_end] < 0) {
+            free_end += 1;
+        }
+        const free_size = free_end - free_beg;
+        if (free_size >= data_size) {
+            for (0..data_size) |i| {
+                const val = data[beg + i];
+                data[beg + i] = data[free_beg + i];
+                data[free_beg + i] = val;
+            }
+            return;
+        } else {
+            // Search for the next free mem
+            free_beg = free_end;
+        }
+    }
 }
